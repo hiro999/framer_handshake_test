0399530e07ad29c1d5fd907d336b755d8dd0fbda
hiro999 <hiro999@mac.com>
2021-10-27 18:22:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Tip } from "./components/Tip";
+
+// A Smart Component from Framer
+import Toggle from "https://framer.com/m/Toggle-B5iT.js@X94mtocAj5XithxLESme";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Toggle />
+
+      <Tip />
+    </div>
+  );
+}
